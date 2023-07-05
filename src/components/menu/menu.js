6e14563dd4c813a5ab9ccda7ec4cfcfccaf5f7cd
unknown <piotrmlodz@gmail.com>
2023-07-05 13:02:52 +0200

    components/menu/menu

---
+++ src/components/menu/menu.js
@@ -0,0 +1,32 @@
+import React, {useContext} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {DataContext} from "../../DataContext";
+
+const Menu = () => {
+    const {setEnteredName, setSelectedCategory, setSelectedDifficulty, setScore} = useContext(DataContext);
+    const navigation = useNavigate();
+    const handleNewGameCLick = () => {
+        setEnteredName("");
+        setSelectedCategory("");
+        setSelectedDifficulty("");
+        setScore(0);
+        navigation("/settings");
+    };
+
+    const handleScoreboardClick = () => {
+        navigation("/scoreboard")
+    }
+
+    return (
+        <>
+            <nav>
+                <button onClick={handleNewGameCLick}>New game</button>
+                <button onClick={handleScoreboardClick}>Scoreboard</button>
+            </nav>
+        </>
+    );
+};
+
+export default Menu;
+
+
