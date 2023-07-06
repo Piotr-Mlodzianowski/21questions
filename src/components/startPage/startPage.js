74e76337cf13ee1feddc727ecde3e1c6aaefa8e4
unknown <piotrmlodz@gmail.com>
2023-07-07 00:22:38 +0200

    components/startPage/startPage_added_bootstrap_classes

--- src/components/startPage/startPage.js
+++ src/components/startPage/startPage.js
@@ -3,14 +3,14 @@ import React from 'react';
 const StartPage = () => {
 
     return (
-        <>
-            <h1>21<span>questions</span></h1>
-            <div>
+        <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+            <h1 className="display-4 text-center">21<span className="text-primary">questions</span></h1>
+            <div className="mt-4">
                 <a href="/settings">
-                    <button>Play</button>
+                    <button className="btn btn-success btn-lg">Play</button>
                 </a>
             </div>
-        </>
+        </div>
     );
 };
 
