d82eac6548567a1b96b1eb3aa9aa6fa6bd4a44cc
unknown <piotrmlodz@gmail.com>
2023-07-09 21:07:35 +0200

    components/scoreboard/scoreboardTable - added_bootstrap_classes

--- src/components/scoreboard/scoreboardTable.js
+++ src/components/scoreboard/scoreboardTable.js
@@ -2,33 +2,37 @@ import React from "react";
 
 const ScoreboardTable = ({data}) => {
     return (
-        <>
-            {!data && <div>Loading</div>}
-            {data &&
-                <>
-                    <table>
-                        <thead>
-                        <tr>
-                            <th>Player</th>
-                            <th>Category</th>
-                            <th>Difficulty</th>
-                            <th>Score</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {data.sort((a, b) => b.score - a.score).map(item => {
-                            return <tr key={item.id}>
-                                <td>{item.name}</td>
-                                <td>{item.category}</td>
-                                <td>{item.difficulty}</td>
-                                <td>{item.score}</td>
+        <div className="container d-flex align-items-center justify-content-center vh-100">
+            <div className="card shadow-sm bg-white rounded p-4">
+                {!data && <div>Loading</div>}
+                {data && (
+                    <>
+                        <table className="table table-striped table-hover">
+                            <thead>
+                            <tr>
+                                <th>Player</th>
+                                <th>Category</th>
+                                <th>Difficulty</th>
+                                <th>Score</th>
                             </tr>
-                        })}
-                        </tbody>
-                    </table>
-                </>}
-
-        </>
+                            </thead>
+                            <tbody>
+                            {data
+                                .sort((a, b) => b.score - a.score)
+                                .map((item) => (
+                                    <tr key={item.id}>
+                                        <td>{item.name}</td>
+                                        <td>{item.category}</td>
+                                        <td>{item.difficulty}</td>
+                                        <td>{item.score}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </>
+                )}
+            </div>
+        </div>
     );
 };
 
