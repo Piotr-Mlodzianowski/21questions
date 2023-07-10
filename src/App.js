beb76d2de205e1548f668dc8ad75c74da2452184
unknown <piotrmlodz@gmail.com>
2023-07-10 21:22:29 +0200

    Edited App with React Bootstrap grid

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import Routing from "./routing"
+import {Container, Row} from 'react-bootstrap';
+import "./components/settings/settings.scss";
+
 
 function App() {
     return (
         <ThemeProvider
-            breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+            breakpoints={['xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
             minBreakpoint="xxs"
         >
-            <Routing/>
+            <Container fluid className="background">
+                <Row className="justify-content-center align-items-center h-100">
+                    <Routing />
+                </Row>
+            </Container>
         </ThemeProvider>
     );
 }
 
+
 export default App;
+
