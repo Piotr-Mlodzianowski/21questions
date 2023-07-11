b26019ed7828a84ff2619ec7a0aa6b76727e9377
unknown <piotrmlodz@gmail.com>
2023-07-11 18:10:32 +0200

    PlayerScore React Bootstrap classes

--- src/components/playerScore/playerScore.js
+++ src/components/playerScore/playerScore.js
@@ -1,20 +1,27 @@
 import React, {useContext,} from 'react';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 import {DataContext} from "../../DataContext";
+import "./playerScore.scss"
 
 
 const PlayerScore = () => {
-    const {score, selectedCategory} = useContext(DataContext);
+    const {score} = useContext(DataContext);
+
+
 
     return (
-        <div className="container d-flex align-items-center justify-content-center vh-100">
-            <div className="card shadow-sm bg-white rounded p-4">
-                <div className="text-center">
-                    <p>Your Score is</p>
-                    <p>{score}</p>
-                    <p>out of 21 questions about {selectedCategory}</p>
-                </div>
-            </div>
-        </div>
+        <Container className="d-flex flex-column align-items-center justify-content-center score__container">
+            <Card className="border-0 shadow p-3 mb-5 rounded score__card">
+                <Card.Body className="d-flex flex-column align-items-center justify-content-center score__cardBody">
+                    <div className="text-center score__text">
+                        <p className="text text__firstLine">Your Score is</p>
+                        <p className="text text__scoreLine">{score} points</p>
+                        <p className="text text__lastLine">out of 21 questions</p>
+                    </div>
+                </Card.Body>
+            </Card>
+        </Container>
     );
 };
 
