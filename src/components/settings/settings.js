a6e609619ff0c8545c4bdaf162bf441e5dd04201
unknown <piotrmlodz@gmail.com>
2023-07-14 19:59:42 +0200

    Settings React Bootstrap, additional scss classes, added loading animation

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {DataContext} from "../../DataContext";
 import SettingsName from "./settingsName";
 import SettingsCategory from "./settingsCategory";
@@ -9,43 +9,67 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import "./settings.scss";
+import "../../scss/loading.scss"
+import ErrorModal from "../errorModal/errorModal";
 
 const Settings = () => {
     const {
+        data,
+        setData,
         enteredName,
-        setEnteredName,
         selectedCategory,
-        setSelectedCategory,
         selectedDifficulty,
-        setSelectedDifficulty
+        showErrorModal,
+        setShowErrorModal
     } = useContext(DataContext);
+
+    const categoryURL = "https://opentdb.com/api_category.php";
+
+    useEffect(() => {
+        fetch(categoryURL, {
+            method: "GET",
+        })
+            .then(response => response.json())
+            .then(data => setData(data.trivia_categories))
+            .catch(err => {
+                console.log(err);
+                setShowErrorModal(true);
+            });
+    }, []);
 
     console.log(enteredName);
     console.log(selectedCategory);
     console.log(selectedDifficulty);
 
-    return (
-        <Container className="d-flex flex-column align-items-center justify-content-center settings__container">
-            <Card className="border-0 shadow p-3 mb-5 rounded settings__card">
-                <Card.Body className="settings__cardBody">
-                    <Form className="d-flex flex-column align-items-center justify-content-center gap-4">
-                        <SettingsName setName={setEnteredName}/>
-                        <SettingsCategory setCategory={setSelectedCategory}/>
-                        <SettingsDifficulty setDifficulty={setSelectedDifficulty}/>
-                        {enteredName && selectedCategory && selectedDifficulty ? (
-                            <SettingsPlayButton/>
-                        ) : (
-                            <Button className="settings__playButton" variant="outline-success" size="lg" disabled>
-                                Play
-                            </Button>
-                        )}
-                    </Form>
-                </Card.Body>
-            </Card>
-        </Container>
+    return (<>
+            {showErrorModal && <ErrorModal/>}
+            {!data && <div className="d-flex flex-column align-items-center justify-content-center loading__container">
+                <div className="loading__animation"></div>
+            </div>}
+            {data &&
+                <Container className="d-flex flex-column align-items-center justify-content-center settings__container">
+                    <Card className="shadow p-3 mb-5 rounded settings__card">
+                        <Card.Body className="settings__cardBody">
+                            <Form className="d-flex flex-column align-items-center justify-content-center gap-4">
+                                <SettingsName/>
+                                <SettingsCategory/>
+                                <SettingsDifficulty/>
+                                {enteredName && selectedCategory && selectedDifficulty ? (
+                                    <SettingsPlayButton/>
+                                ) : (
+                                    <Button className="settings__playButton" variant="outline-success" size="lg"
+                                            disabled>
+                                        Play
+                                    </Button>
+                                )}
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Container>
+            }
+        </>
     );
 };
-
 
 
 export default Settings;
