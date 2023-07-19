3f5ee88534e7b2f193204cca581e4180f622ce65
unknown <piotrmlodz@gmail.com>
2023-07-19 20:18:10 +0200

    firebase realtime database

--- src/components/scoreboard/scoreboardFetch.js
+++ src/components/scoreboard/scoreboardFetch.js
@@ -1,36 +1,45 @@
-import React, {useContext, useEffect, useState} from 'react';
-import ScoreboardTable from './scoreboardTable'
-import {DataContext} from "../../DataContext";
-import ErrorModal from "../errorModal/errorModal";
-//json-server database/db.json --watch
+import React, { useContext, useEffect, useState } from 'react';
+import ScoreboardTable from './scoreboardTable';
+import { DataContext } from '../../DataContext';
+import ErrorModal from '../errorModal/errorModal';
+import { ref, get, orderByChild, query } from 'firebase/database';
+import { db } from '../../index.js';
 
 const ScoreboardFetch = () => {
-    const {showErrorModal, setShowErrorModal, setFetchedScores} = useContext(DataContext);
+    const { showErrorModal, setShowErrorModal, setFetchedScores } = useContext(DataContext);
     const [data, setData] = useState(false);
 
-    const scoreURL = "http://localhost:3000/score"
-
     useEffect(() => {
-        fetch(scoreURL, {
-            method: "GET",
-        })
-            .then(response => response.json())
-            .then(data => {
-                setFetchedScores(data);
-                setData(true);
+        const scoresRef = ref(db, 'scores');
+
+        // Create a query to order by a child key ('score' in this case) to get the data in ascending order of score
+        const scoresQuery = query(scoresRef, orderByChild('score'));
+
+        // Fetch data once from the database
+        get(scoresQuery)
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                    const scoresData = Object.values(snapshot.val());
+                    setFetchedScores(scoresData);
+                    setData(true);
+                } else {
+                    console.log('No data available');
+                }
             })
-            .catch(err => {
-                console.log(err);
+            .catch((error) => {
+                console.error('Error retrieving scores:', error);
                 setShowErrorModal(true);
             });
-    }, [])
+
+        // No need to set up a listener, as get fetches data once
+
+    }, [setFetchedScores, setShowErrorModal]);
 
     return (
         <>
-            {showErrorModal && <ErrorModal/>}
-            {data && <ScoreboardTable/>}
+            {showErrorModal && <ErrorModal />}
+            {data && <ScoreboardTable />}
         </>
-
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./scss/index.scss"
 import App from './App';
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDdWzuBu-Hi-1j5p31M2a0vj33-tVhOVc8",
+    authDomain: "questions-cc348.firebaseapp.com",
+    databaseURL: "https://questions-cc348-default-rtdb.firebaseio.com",
+    projectId: "questions-cc348",
+    storageBucket: "questions-cc348.appspot.com",
+    messagingSenderId: "910510601669",
+    appId: "1:910510601669:web:cb057c77fee6ba83db87ac"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
